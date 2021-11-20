81460c26bf6e535fa6149995205689369bd7a84d
Rahimkulov <q3032002@gmail.com>
2021-11-20 20:37:19 +0500

    1 и 2 лаба

---
+++ labaoop1/labaoop1/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace labaoop1
+{
+    partial class Form1
+    {
+
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.Pagetext = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.indexlist = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timerstart = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.indexlist)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "цвет ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AllowDrop = true;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.Crimson;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(65, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Disco";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(20, 352);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(20, 398);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 10;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.InitialImage = global::labaoop1.Properties.Resources.a01eb920157d569f0c214bc48ef1dec4;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 204);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(694, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(626, 366);
+            this.tabControl1.TabIndex = 13;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webBrowser1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(618, 337);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "guide";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(17, 6);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(595, 325);
+            this.webBrowser1.TabIndex = 19;
+            this.webBrowser1.Url = new System.Uri("https://www.youtube.com/watch?v=BEmQpbVHjwg", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(618, 337);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Страница 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // Pagetext
+            // 
+            this.Pagetext.AutoSize = true;
+            this.Pagetext.Location = new System.Drawing.Point(691, 381);
+            this.Pagetext.Name = "Pagetext";
+            this.Pagetext.Size = new System.Drawing.Size(152, 17);
+            this.Pagetext.TabIndex = 0;
+            this.Pagetext.Text = "вы на странице гайда";
+            this.Pagetext.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "первый",
+            "второй",
+            "третий",
+            "четвертый",
+            "пятый"});
+            this.listBox1.Location = new System.Drawing.Point(267, 109);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(220, 84);
+            this.listBox1.TabIndex = 14;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // indexlist
+            // 
+            this.indexlist.Location = new System.Drawing.Point(267, 81);
+            this.indexlist.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.indexlist.Name = "indexlist";
+            this.indexlist.Size = new System.Drawing.Size(120, 22);
+            this.indexlist.TabIndex = 15;
+            this.indexlist.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.indexlist.ValueChanged += new System.EventHandler(this.indexlist_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(393, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timerstart
+            // 
+            this.timerstart.AutoSize = true;
+            this.timerstart.Location = new System.Drawing.Point(343, 445);
+            this.timerstart.Name = "timerstart";
+            this.timerstart.Size = new System.Drawing.Size(92, 21);
+            this.timerstart.TabIndex = 17;
+            this.timerstart.Text = "start timer";
+            this.timerstart.UseVisualStyleBackColor = true;
+            this.timerstart.CheckedChanged += new System.EventHandler(this.timerstart_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(343, 472);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(33, 23);
+            this.panel1.TabIndex = 18;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(382, 472);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(650, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(20, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "убрать картинку с фона";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(393, 272);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "создать кнопку";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1402, 629);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.timerstart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.indexlist);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Pagetext);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.indexlist)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label Pagetext;
+        private System.Windows.Forms.NumericUpDown indexlist;
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton timerstart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ labaoop1/labaoop1/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labaoop1
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int tick = 30;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                int Height = ClientSize.Height;
+                int Width = ClientSize.Width;
+                this.BackColor = System.Drawing.Color.FromArgb((e.X * 255 / Width)%255, (e.Y * 255 / Height)%255, e.Y * 127 / Height + e.X * 127 / Width);
+            }
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            dateTimePicker1.Value= e.End;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = global::labaoop1.Properties.Resources.a01eb920157d569f0c214bc48ef1dec4;
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+            Pagetext.Text = "вы на странице 2";
+            webBrowser1.Update();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+            Pagetext.Text = "вы на странице гайда";
+        }
+
+        private void indexlist_ValueChanged(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex= (int)indexlist.Value;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex > -1) 
+            { 
+                listBox1.Items[listBox1.SelectedIndex] = textBox1.Text;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value > 99) 
+            { 
+                panel1.Size = new System.Drawing.Size(panel1.Size.Width-tick, panel1.Size.Height-tick);
+                tick = -tick;
+                panel1.BackColor = System.Drawing.Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+            }else progressBar1.Value++;
+            
+        }
+
+        private void timerstart_CheckedChanged(object sender, EventArgs e)
+        {
+          timer1.Enabled = true;
+           
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = null;
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Button btn = new System.Windows.Forms.Button();
+            btn.Location = new System.Drawing.Point(random.Next(0,Width),random.Next(0,Height));
+            btn.BackColor = System.Drawing.Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+            this.Controls.Add(btn);
+        }
+    }
+}
